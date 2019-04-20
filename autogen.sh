4fd17fc413cea8346bc4ad9af58db565c2fb1ef3
Emily23805 <33092539+Emily23805@users.noreply.github.com>
2019-04-20 19:47:58 +0900

    Update autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,9 @@ export CPPFLAGS
 export CFLAGS
 export LDFLAGS
 
+echo "rpm 패키지 메니저를 설치하실 디렉터리를 골라주세요 ex) /root/rpm/bin"
+read install
+
 autoreconf -i
 
 case "$1" in
@@ -15,6 +18,6 @@ case "$1" in
     eval "`rpm --eval %configure`" "$@"
     ;;
   *)
-    ./configure "$@"
+    ./configure --prefix=$install "$@"
     ;;
 esac
